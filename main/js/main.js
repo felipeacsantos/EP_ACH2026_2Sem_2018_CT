ea3c44254d9a3a554e33ea9b122f953605c83530
Felipe Augusto <felipe.dossantos@fluig.com>
2018-10-14 01:29:26 -0300

    Initial implementation of new board

--- main/js/main.js
+++ main/js/main.js
@@ -2,6 +2,10 @@ const Jogo = function () {
     let roomId;
     var socket = io();
     let type;
+
+    var casa_selecionada = null;
+    var peca_selecionada = null;
+
     $('.board').hide();
     $('.back').hide();
     $('.message').hide();
@@ -83,14 +87,48 @@ const Jogo = function () {
         socket.emit('check', { type: type, row: $(this).attr('data-row'), col: $(this).attr('data-col') })
     })
 
-    function renderBoard(board) {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 0) {
+    $(".casa").click(function(){
+        $("#"+casa_selecionada).removeClass("casa_selecionada");
+        casa_selecionada = $(this).attr("id");
+        $("#"+casa_selecionada).addClass("casa_selecionada");
+        $("#info_casa_selecionada").text(casa_selecionada);
+     
+        peca_selecionada = $("#"+casa_selecionada).children("img:first").attr("id");
+        if(peca_selecionada==null){
+            peca_selecionada = "NENHUMA PECA SELECIONADA";
+        }
+        $("#info_peca_selecionada").text(peca_selecionada.toString());
+    })
 
-                } else {
-                    board[i][j] == 1 ? board[i][j] = 'O' : board[i][j] = 'X'
-                    $('td[data-row=' + i + '][data-col=' + j + ']').append('<div class="check">' + board[i][j] + '</div>')
+    function renderBoard(board) {
+        // for (let i = 0; i < board.length; i++) {
+        //     for (let j = 0; j < board[i].length; j++) {
+        //         if (board[i][j] == 0) {
+
+        //         } else {
+        //             board[i][j] == 1 ? board[i][j] = 'O' : board[i][j] = 'X'
+        //             $('td[data-row=' + i + '][data-col=' + j + ']').append('<div class="check">' + board[i][j] + '</div>')
+        //         }
+        //     }
+        // }
+        var i;
+        for (i=0; i<8; i++){
+            $("#tabuleiro").append("<div id='linha_"+i.toString()+"' class='linha' >");       
+     
+            for (j=0; j<8; j++){
+                var nome_casa ="casa_"+i.toString()+"_"+j.toString();
+                var classe = (i%2==0?(j%2==0?"casa_branca":"casa_preta"):(j%2!=0?"casa_branca":"casa_preta"));
+                $("#linha_"+i.toString()).append("<div id='"+nome_casa+"' class='casa "+classe+"' />");
+     
+                if(classe == "casa_preta"){
+                    if(i < 3){
+                        $("#"+nome_casa).append("<img src='peca_preta.png' class='peca' id='"+nome_casa.replace("casa", "peca_preta")+"'/>");
+                    }
+                    else
+                    if(i > 4){
+                        $("#"+nome_casa).append("<img src='peca_branca.svg' class='peca' id='"+nome_casa.replace("casa", "peca_branca")+"'/>");   
+                    }
+     
                 }
             }
         }
@@ -105,4 +143,38 @@ const Jogo = function () {
             }
         }
     }
+}
+
+function showBoard(){
+    var i;
+        for (i=0; i<8; i++){
+            $("#tabuleiro").append("<div id='linha_"+i.toString()+"' class='linha' >");       
+     
+            for (j=0; j<8; j++){
+                var nome_casa ="casa_"+i.toString()+"_"+j.toString();
+                var classe = (i%2==0?(j%2==0?"casa_branca":"casa_preta"):(j%2!=0?"casa_branca":"casa_preta"));
+                $("#linha_"+i.toString()).append("<div id='"+nome_casa+"' class='casa "+classe+"' />");
+     
+                if(classe == "casa_preta"){
+                    if(i < 3){
+                        $("#"+nome_casa).append("<img src='peca_preta.png' class='peca' id='"+nome_casa.replace("casa", "peca_preta")+"'/>");
+                    }
+                    else
+                    if(i > 4){
+                        $("#"+nome_casa).append("<img src='peca_branca.svg' class='peca' id='"+nome_casa.replace("casa", "peca_branca")+"'/>");   
+                    }
+     
+                }
+                else if(classe == "casa_branca"){
+                    if(i < 3){
+                        $("#"+nome_casa).append("<img src='peca_preta.png' class='peca' id='"+nome_casa.replace("casa", "peca_preta")+"'/>");
+                    }
+                    else
+                    if(i > 4){
+                        $("#"+nome_casa).append("<img src='peca_branca.svg' class='peca' id='"+nome_casa.replace("casa", "peca_branca")+"'/>");   
+                    }
+     
+                }
+            }
+        }
 }
